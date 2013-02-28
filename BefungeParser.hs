7e15c2e8b2b4c87246d2372d44207a74778a8816
Kajsa Goffrich and Rickard Lindberg <kajsa.goffrich+ricli85@gmail.com>
2013-02-28 16:39:22 +0100

    Refactor program representation.

--- BefungeParser.hs
+++ BefungeParser.hs
@@ -1,19 +1,37 @@
 -- @+leo-ver=5-thin
 -- @+node:kajsa.20130215180649.1361: * @file BefungeParser.hs
 -- @+others
+-- @+node:kajsa.20130228155956.1376: ** Program abstraction
+data Program = Program String
+
+data ProgramCounter = ProgramCounter Int
+
+instructionAt :: Program -> ProgramCounter -> Char
+instructionAt (Program p) (ProgramCounter pc) = p !! pc
+
+calculateNextPc :: Program -> ProgramCounter -> ProgramCounter
+calculateNextPc (Program p) (ProgramCounter pc) = ProgramCounter (pc + 1)
 -- @+node:kajsa.20130215183429.1369: ** Interpreter
 runBefungeProgram :: String -> String
-runBefungeProgram program = interpretProgram program []
+runBefungeProgram program = interpretProgram (Program program) (ProgramCounter 0) []
 
-interpretProgram :: String -> String -> String
-interpretProgram ('"':xs)       stack     = interpretString xs stack
-interpretProgram (',':xs)       (s:stack) = s:(interpretProgram xs stack)
-interpretProgram ('@':xs)       stack     = ""
-interpretProgram   (x:xs)       stack     = interpretProgram xs stack
+interpretProgram :: Program -> ProgramCounter -> String -> String
+interpretProgram p pc stack =
+    case instructionAt p pc of
+        '"' -> interpretString p nextPc stack
+        ',' -> (head stack):(interpretProgram p nextPc (tail stack))
+        '@' -> ""
+        _ -> interpretProgram p nextPc stack
+    where
+        nextPc = calculateNextPc p pc
 
-interpretString :: String -> String -> String
-interpretString ('"':xs) stack = interpretProgram xs stack
-interpretString (c:xs) stack = interpretString xs (c:stack)
+interpretString :: Program -> ProgramCounter -> String -> String
+interpretString p pc stack =
+    case instructionAt p pc of
+        '"' -> interpretProgram p nextPc stack
+        c -> interpretString p nextPc (c:stack)
+    where
+        nextPc = calculateNextPc p pc
 -- @+node:kajsa.20130228131825.1375: ** Run program
 assertGivesOutput :: String -> String -> IO ()
 assertGivesOutput program expectedOutput =
@@ -26,5 +44,8 @@ assertGivesOutput program expectedOutput =
 main = do
     assertGivesOutput "\"!\",@" "!"
     assertGivesOutput "\"!dlroW olleH\",,,,,,,,,,,,@" "Hello World!"
+--    assertGivesOutput (">                            v\n" ++
+--                       "@ ,,,,,,,,,,,,\"Hello World!\" <")
+--                      "Hello World!"
 -- @-others
 -- @-leo
